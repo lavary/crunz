9095456f9c5fea99f2f426e055eb43db79821743
Oliver Voros <olivervoros@gmail.com>
2016-04-29 19:27:11 +0200

    Implementation of task number and force Console command option

--- src/Console/Commands/ScheduleRunCommand.php
+++ src/Console/Commands/ScheduleRunCommand.php
@@ -37,12 +37,20 @@ class ScheduleRunCommand extends Command
 
         'src' => '/tasks',
     ];
+	
+	/**
+     * Console Output
+     *
+     * @var Symfony\Component\Console\Input\OutputIterface $output
+     */
+	protected $output;
+	
 
     /**
      * Configures the current command
      *
      */
-     protected function configure()
+    protected function configure()
      {
         $this->setName('schedule:run')
              ->setDescription('Start the scheduler')
@@ -68,25 +76,61 @@ class ScheduleRunCommand extends Command
         $this->options   = $input->getOptions();
         $src             = $this->arguments['source'];
         $task_files      = TaskfileFinder::collectFiles($src); 
-		$task			 = $this->options['task']; 
-				    
-        if (!count($task_files)) {
-            $output->writeln('<comment>No task found!</comment>');
-            exit();
-        }
+		$requsted_task	 = $this->options['task']; 
+		$force			 = (bool) $this->options['force']; 
+		$this->output	 = $output;
 		
-        $running_events = [];
-        
-        foreach ($task_files as $key => $taskFile) {
-                        
-            $schedule = require $taskFile->getRealPath();            
-            if (!$schedule instanceof Schedule) {
-                continue;
-            } 
+		$events = $this->getEvents($task_files, $requsted_task, $force);
+		$this->runEvents($events);        
+      
+    }
+	
+		/**
+		 * Gets all the events
+		 *
+		 * @param $task_files
+		 * @param $requsted_task If this is set the method only return the requested task
+		 * @param $force If true, this method returns all events otherwise it only returns due events
+		 *
+		 * @return array Returns all the events as requested
+		 */
+		protected function getEvents($task_files, $requsted_task, $force) {
+		  if (!count($task_files))
+		  {
+			$this->outputComment('No task found!');
+		  }
 
-            $events = $schedule->dueEvents(new Invoker());   
-		foreach ($events as $event) {
-				
+		foreach ($task_files as $taskFile) {
+
+		  $schedule = require $taskFile->getRealPath();
+			if (!$schedule instanceof Schedule)
+			{
+			  continue;
+			}
+
+			$events = $schedule->events();
+			$dueEvents = $schedule->dueEvents(new Invoker());
+
+		}
+		if($requsted_task !== null) {
+		  return $this->getTask($events, $dueEvents, $requsted_task, $force);
+		}
+		else {
+		  return ($force) ? $events : $dueEvents;
+		}
+
+	  }
+  
+  /**
+     * Runs all the events
+     *
+     * @param $events
+     * @return void
+     */
+  protected function runEvents($events) {
+	  $running_events = [];
+	  foreach ($events as $event) {
+		 		
                 echo '[', date('Y-m-d H:i:s'), '] Running scheduled command: ', $event->getSummaryForDisplay(), PHP_EOL;
                 echo $event->buildCommand(), PHP_EOL;
                 echo '---', PHP_EOL;
@@ -95,11 +139,9 @@ class ScheduleRunCommand extends Command
                 $running_events[] = $event->callBeforeCallbacks(new Invoker())
                                           ->run(new Invoker());   
 		}
-        }
 		
-        if (!count($running_events)) {
-            $output->writeln('<comment>No task is due!</comment>');
-            exit();
+        if (!count($running_events)) {		  
+			$this->outputComment('No task is due!');
         }
 
         // Running the post-execution hooks
@@ -113,6 +155,38 @@ class ScheduleRunCommand extends Command
                 unset($running_events[$key]);
             }
         }
-    }
+	}
+	
+	/**
+     * Gets the requested task
+     *
+     * @param $events  All available events
+     * @param $dueEvents  All due events
+	 * @param $requested_task The number of requested task, as it is listed using schedule:list
+     *
+     * @return array Returns the requested event, exit with error message otherwise
+     */
+	protected function getTask($events, $dueEvents, $requsted_task, $force) {
+		if (empty($events[$requsted_task - 1]))
+		{
+		  $this->outputComment('The requested task does not exists.');
+		}
+		elseif (!empty($events[$requsted_task - 1]) && empty($dueEvents[$requsted_task - 1]) && !$force)
+		{
+		  $this->outputComment('The requested task exists but it is not due.');
+		}
+		else
+		{
+		  return [$events[$requsted_task - 1]];
+		}
+  }
   
+	protected function outputComment($comment = '') {
+	if (isset($this->output))
+	{
+	  $this->output->writeln("<comment>$comment</comment>");
+	}
+	exit();
+  }
+
 }
