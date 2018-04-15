a37b9d0dbe4a49954a2557425db5b6af671dc7ac
pkowalczyk <11366345+PabloKowalczyk@users.noreply.github.com>
2018-04-15 09:55:57 +0200

    Introduce DI container in Kernel.

--- src/Console/CommandKernel.php
+++ src/Console/CommandKernel.php
@@ -2,7 +2,13 @@
 
 namespace Crunz\Console;
 
+use Symfony\Component\Config\ConfigCache;
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Console\Application as SymfonyApplication;
+use Symfony\Component\DependencyInjection\Container;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 class CommandKernel extends SymfonyApplication
 {
@@ -11,15 +17,13 @@ class CommandKernel extends SymfonyApplication
      *
      * @var array
      */
-    protected $commands = [
-        
+    const COMMANDS = [
         // This command starts the event runner (vendor/bin/crunz schedule:run)
         // It takes an optional argument which is the source directory for tasks
         // If the argument is not provided, the default in the configuratrion file
         // will be considered as the source path
         \Crunz\Console\Command\ScheduleRunCommand::class,
-        
-        
+
         // This command (vendor/bin/schedule:list) lists the scheduled events in different task files
         // Just like schedule:run it gets the :source argument
         \Crunz\Console\Command\ScheduleListCommand::class,
@@ -29,8 +33,7 @@ class CommandKernel extends SymfonyApplication
         // This is often useful when you want to create a task file and start
         // adding tasks to it.
         \Crunz\Console\Command\TaskGeneratorCommand::class,
-        
-        
+
         // The modify the configuration, the user's own copy should be modified
         // This command creates a configuration file in Crunz installation directory
         \Crunz\Console\Command\ConfigGeneratorCommand::class,
@@ -38,8 +41,10 @@ class CommandKernel extends SymfonyApplication
         // This command is used by Crunz itself for running serialized closures
         // It accepts an argument which is the serialized form of the closure to run.
         \Crunz\Console\Command\ClosureRunCommand::class,
-        
     ];
+
+    /** @var Container */
+    private $container;
 
     /**
      * Instantiate the class
@@ -48,22 +53,93 @@ class CommandKernel extends SymfonyApplication
     public function __construct($appName, $appVersion)
     {
         parent::__construct($appName, $appVersion);
-        
-        foreach($this->commands as $command) {
-            
-            $this->add(new $command);
 
-        }   
+        $this->initializeContainer();
+        
+        foreach(self::COMMANDS as $commandClass) {
+            $command = $this->container
+                ->get($commandClass)
+            ;
+
+            $this->add($command);
+        }
+    }
+
+    private function initializeContainer()
+    {
+        $class = 'CrunzContainer';
+        $baseClass = 'Container';
+        $cache = new ConfigCache(
+            implode(
+                DIRECTORY_SEPARATOR,
+                [
+                    $this->getCacheDir(),
+                    "{$class}.php"
+                ]
+            ),
+            false
+        );
+
+        if (!$cache->isFresh()) {
+            $containerBuilder = $this->buildContainer();
+            $containerBuilder->compile();
+
+            $this->dumpContainer(
+                $cache,
+                $containerBuilder,
+                $class,
+                $baseClass
+            );
+        }
+
+        require_once $cache->getPath();
+
+        $this->container = new $class();
     }
 
     /**
-     * Run the command
-     *
-     * @param array $arguments
+     * @return ContainerBuilder
+     * @throws \Exception
      */
-    public function handle()
-    {    
-        $this->run();
+    private function buildContainer()
+    {
+        $containerBuilder = new ContainerBuilder();
+        $containerBuilder->setParameter('crunz.root_dir', CRUNZ_ROOT);
+
+        $loader = new XmlFileLoader($containerBuilder, new FileLocator(CRUNZ_ROOT . '/config'));
+        $loader->load('services.xml');
+
+        return $containerBuilder;
     }
 
+    private function dumpContainer(
+        ConfigCache $cache,
+        ContainerBuilder $container,
+        $class,
+        $baseClass
+    ) {
+        $dumper = new PhpDumper($container);
+
+        $content = $dumper->dump(
+            [
+                'class' => $class,
+                'base_class' => $baseClass,
+                'file' => $cache->getPath()
+            ]
+        );
+
+        $cache->write($content, $container->getResources());
+    }
+
+    private function getCacheDir()
+    {
+        return implode(
+            DIRECTORY_SEPARATOR,
+            [
+                CRUNZ_ROOT,
+                'var',
+                'cache'
+            ]
+        );
+    }
 }
