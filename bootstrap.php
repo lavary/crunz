935ae092ea8558268b31cb6849aea676adad740f
pkowalczyk <11366345+PabloKowalczyk@users.noreply.github.com>
2018-05-10 21:00:08 +0200

    Put envvar CRUNZ_BASE_DIR in test env.

--- bootstrap.php
+++ bootstrap.php
@@ -2,4 +2,13 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-define('CRUNZ_ROOT', __DIR__);
+$testCrunzRoot = \implode(
+    DIRECTORY_SEPARATOR,
+    [
+        __DIR__,
+        'tests'
+    ]
+);
+
+\define('CRUNZ_ROOT', $testCrunzRoot);
+\putenv('CRUNZ_BASE_DIR=' . $testCrunzRoot);
