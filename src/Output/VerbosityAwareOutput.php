79c6a4854a36887ccdbd1b90231f7c50b96dcc64
pkowalczyk <11366345+PabloKowalczyk@users.noreply.github.com>
2018-05-27 08:13:32 +0200

    Added VerbosityAwareOutput.

---
+++ src/Output/VerbosityAwareOutput.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Crunz\Output;
+
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @TODO Remove it in Crunz v2.
+ */
+class VerbosityAwareOutput
+{
+    /** @var OutputInterface */
+    private $output;
+
+    public function __construct(OutputInterface $output)
+    {
+        $this->output = $output;
+    }
+
+    public function write($messages, $verbosity = OutputInterface::VERBOSITY_NORMAL)
+    {
+        $commandVerbosity = $this->output
+            ->getVerbosity()
+        ;
+
+        if ($commandVerbosity >= $verbosity) {
+            $this->output
+                ->write($messages)
+            ;
+        }
+    }
+
+    public function writeln($messages, $verbosity = OutputInterface::VERBOSITY_NORMAL)
+    {
+        $commandVerbosity = $this->output
+            ->getVerbosity()
+        ;
+
+        if ($commandVerbosity >= $verbosity) {
+            $this->output
+                ->writeln($messages)
+            ;
+        }
+    }
+}
