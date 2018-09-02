42df062a828c055bbbf1d10848bdba73f106d1b6
pkowalczyk <11366345+PabloKowalczyk@users.noreply.github.com>
2018-09-02 10:55:25 +0200

    Add Pinger.

---
+++ src/Pinger/PingableInterface.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Crunz\Pinger;
+
+interface PingableInterface
+{
+    /**
+     * @param string $url
+     *
+     * @return $this
+     */
+    public function pingBefore($url);
+
+    /**
+     * @return bool
+     *
+     * @internal
+     */
+    public function hasPingBefore();
+
+    /**
+     * @param string $url
+     *
+     * @return $this
+     */
+    public function thenPing($url);
+
+    /**
+     * @return bool
+     *
+     * @internal
+     */
+    public function hasPingAfter();
+
+    /**
+     * @return string
+     *
+     * @internal
+     */
+    public function getPingBeforeUrl();
+
+    /**
+     * @return string
+     *
+     * @internal
+     */
+    public function getPingAfterUrl();
+}
---
+++ tests/Unit/Pingable.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Crunz\Tests\Unit;
+
+use Crunz\Pinger\PingableInterface;
+use Crunz\Pinger\PingableTrait;
+
+class Pingable implements PingableInterface
+{
+    use PingableTrait;
+}
