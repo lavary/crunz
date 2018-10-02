a0537b2e4698ada138ff394f31768e4e277ed1c8
pkowalczyk <11366345+PabloKowalczyk@users.noreply.github.com>
2018-10-02 22:11:00 +0200

    Add OutputFactory which configures verbosity on create.

---
+++ src/Output/OutputFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Crunz\Output;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Output\OutputInterface;
+
+final class OutputFactory
+{
+    /** @var InputInterface */
+    private $input;
+
+    public function __construct(InputInterface $input)
+    {
+        $this->input = $input;
+    }
+
+    public function createOutput()
+    {
+        $input = $this->input;
+        $output = new ConsoleOutput();
+
+        if (true === $input->hasParameterOption(['--quiet', '-q'])) {
+            $output->setVerbosity(OutputInterface::VERBOSITY_QUIET);
+        } else {
+            if ($input->hasParameterOption('-vvv') || $input->hasParameterOption('--verbose=3') || 3 === $input->getParameterOption('--verbose')) {
+                $output->setVerbosity(OutputInterface::VERBOSITY_DEBUG);
+            } elseif ($input->hasParameterOption('-vv') || $input->hasParameterOption('--verbose=2') || 2 === $input->getParameterOption('--verbose')) {
+                $output->setVerbosity(OutputInterface::VERBOSITY_VERY_VERBOSE);
+            } elseif ($input->hasParameterOption('-v') || $input->hasParameterOption('--verbose=1') || $input->hasParameterOption('--verbose') || $input->getParameterOption('--verbose')) {
+                $output->setVerbosity(OutputInterface::VERBOSITY_VERBOSE);
+            }
+        }
+
+        return $output;
+    }
+}
