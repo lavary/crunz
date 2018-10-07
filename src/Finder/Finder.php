e399cd57808f3311dd5c3b09e67bbb2c13146dc0
pkowalczyk <11366345+PabloKowalczyk@users.noreply.github.com>
2018-10-07 15:17:30 +0200

    Introduce Finder as a replacement for Symfony\Finder.

---
+++ src/Finder/Finder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Crunz\Finder;
+
+use Crunz\Path\Path;
+
+final class Finder implements FinderInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function find(Path $path)
+    {
+        return new \GlobIterator(
+            $path->toString(),
+            \FilesystemIterator::CURRENT_AS_FILEINFO | \FilesystemIterator::SKIP_DOTS
+        );
+    }
+}
