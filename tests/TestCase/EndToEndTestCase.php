407429b51c52c7bd4789f7f8b6779cd2c38ce7c5
PabloKowalczyk <11366345+PabloKowalczyk@users.noreply.github.com>
2019-01-19 13:31:41 +0100

    Add createEnvironmentBuilder to EndToEnd test case

--- tests/TestCase/EndToEndTestCase.php
+++ tests/TestCase/EndToEndTestCase.php
@@ -2,11 +2,17 @@
 
 namespace Crunz\Tests\TestCase;
 
+use Crunz\Filesystem\Filesystem;
+use Crunz\Filesystem\FilesystemInterface;
+use Crunz\Tests\TestCase\EndToEnd\Environment\EnvironmentBuilder;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Process\Process;
 
 abstract class EndToEndTestCase extends TestCase
 {
+    /** @var FilesystemInterface */
+    private $filesystem;
+
     /**
      * @param string $command
      *
@@ -21,5 +27,15 @@ abstract class EndToEndTestCase extends TestCase
         }
 
         return $process;
+    }
+
+    /** @return EnvironmentBuilder */
+    public function createEnvironmentBuilder()
+    {
+        if (null === $this->filesystem) {
+            $this->filesystem = new Filesystem();
+        }
+
+        return new EnvironmentBuilder($this->filesystem);
     }
 }
