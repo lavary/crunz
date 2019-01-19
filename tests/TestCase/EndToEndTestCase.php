29ea09d94647b0326973f643d02727c058ad8f58
PabloKowalczyk <11366345+PabloKowalczyk@users.noreply.github.com>
2019-01-19 15:57:41 +0100

    Remove unused "createProcess" method

--- tests/TestCase/EndToEndTestCase.php
+++ tests/TestCase/EndToEndTestCase.php
@@ -6,28 +6,11 @@ use Crunz\Filesystem\Filesystem;
 use Crunz\Filesystem\FilesystemInterface;
 use Crunz\Tests\TestCase\EndToEnd\Environment\EnvironmentBuilder;
 use PHPUnit\Framework\TestCase;
-use Symfony\Component\Process\Process;
 
 abstract class EndToEndTestCase extends TestCase
 {
     /** @var FilesystemInterface */
     private $filesystem;
-
-    /**
-     * @param string $command
-     *
-     * @return Process
-     */
-    public function createProcess($command)
-    {
-        if (\method_exists(Process::class, 'fromShellCommandline')) {
-            $process = Process::fromShellCommandline($command);
-        } else {
-            $process = new Process($command);
-        }
-
-        return $process;
-    }
 
     /** @return EnvironmentBuilder */
     public function createEnvironmentBuilder()
