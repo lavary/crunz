f026da2db60befd17eb11d75130bd31928015b1f
PabloKowalczyk <11366345+PabloKowalczyk@users.noreply.github.com>
2019-02-23 09:43:18 +0100

    Use NullLogger instead of mocked one

--- tests/Unit/Configuration/ConfigurationParserTest.php
+++ tests/Unit/Configuration/ConfigurationParserTest.php
@@ -9,7 +9,7 @@ use Crunz\Configuration\ConfigurationParser;
 use Crunz\Configuration\Definition;
 use Crunz\Configuration\FileParser;
 use Crunz\Filesystem\FilesystemInterface;
-use Crunz\Logger\ConsoleLoggerInterface;
+use Crunz\Tests\TestCase\Logger\NullLogger;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Config\Definition\Processor;
 
@@ -66,7 +66,7 @@ final class ConfigurationParserTest extends TestCase
             $definition,
             $definitionProcessorMock,
             $fileParser,
-            $this->createMock(ConsoleLoggerInterface::class),
+            new NullLogger(),
             $filesystemMock
         );
     }
