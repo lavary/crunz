119d4e31bf586a95f0580af43024fa167dba3044
PabloKowalczyk <11366345+PabloKowalczyk@users.noreply.github.com>
2019-04-12 16:11:49 +0200

    Fix AppVeyor build issues

--- tests/EndToEnd/WrongTaskTest.php
+++ tests/EndToEnd/WrongTaskTest.php
@@ -27,7 +27,7 @@ final class WrongTaskTest extends EndToEndTestCase
 
         $this->assertFalse($process->isSuccessful());
         $this->assertRegExp(
-            "@Task at path '.*WrongTasks.php' returned 'array', but 'Crunz\\\\Schedule' instance is required\.@",
+            "@Task at path '.*WrongTasks\\.php' returned 'array', but 'C( ?)runz\\\\Schedule' instance is required\.@",
             $normalizedOutput
         );
     }
