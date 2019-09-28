74c5b1453b53de637c119a3ef7dc9e4e2ab0b3ad
PabloKowalczyk <11366345+PabloKowalczyk@users.noreply.github.com>
2019-09-28 10:28:00 +0200

    Add comments

--- bootstrap.php
+++ bootstrap.php
@@ -2,11 +2,12 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+// Disable deprecation helper
 $envFlags = new \Crunz\EnvFlags\EnvFlags();
 $envFlags->disableDeprecationHandler();
 
+// Make sure current working directory is "tests"
 $filesystem = new \Crunz\Filesystem\Filesystem();
-
 if (\strpos($filesystem->getCwd(), 'tests') !== false) {
     return;
 }
