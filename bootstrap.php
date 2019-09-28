d64c329667cf067e30313399d1c6e25b62aa57cd
PabloKowalczyk <11366345+PabloKowalczyk@users.noreply.github.com>
2019-09-28 10:30:38 +0200

    Add monkey patches to avoid "Function ReflectionType::__toString() is deprecated" warnings

--- bootstrap.php
+++ bootstrap.php
@@ -15,3 +15,18 @@ if (\strpos($filesystem->getCwd(), 'tests') !== false) {
 if (!\chdir('tests')) {
     throw new RuntimeException("Unable to change current directory to 'tests'.");
 }
+
+// Money patch PHPUnit_Framework_MockObject_Generator to avoid
+// "Function ReflectionType::__toString() is deprecated" warnings
+$mockObjectVersion = PHP_VERSION_ID >= 70100
+    ? '71'
+    : 'org'
+;
+$mockObjectFilePath = __DIR__ . "/tests/resources/patches/php-mock-object-{$mockObjectVersion}.php";
+
+if (PHP_VERSION_ID >= 70100) {
+    \file_put_contents(
+        __DIR__ . '/vendor-bin/phpunit/vendor/phpunit/phpunit-mock-objects/src/Framework/MockObject/Generator.php',
+        \file_get_contents($mockObjectFilePath)
+    );
+}
