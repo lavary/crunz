e2eeb0769b042c7bbb01f1ec5a6997df48f39511
PabloKowalczyk <11366345+PabloKowalczyk@users.noreply.github.com>
2020-04-13 12:48:53 +0200

    Add "\Crunz\Tests\TestCase\UnitTestCase" class

---
+++ tests/TestCase/UnitTestCase.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Crunz\Tests\TestCase;
+
+use Crunz\Application\Service\ClosureSerializerInterface;
+use Crunz\Infrastructure\Opis\Closure\OpisClosureSerializer;
+use PHPUnit\Framework\TestCase;
+
+abstract class UnitTestCase extends TestCase
+{
+    /** @var ClosureSerializerInterface|null */
+    private $closureSerializer = null;
+
+    public function createClosureSerializer(): ClosureSerializerInterface
+    {
+        if (null === $this->closureSerializer) {
+            $this->closureSerializer = new OpisClosureSerializer();
+        }
+
+        return $this->closureSerializer;
+    }
+}
