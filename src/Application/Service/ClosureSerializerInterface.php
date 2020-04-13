af54bddc7ef8f5f6c3f6e3e43a03bdc233369f9c
PabloKowalczyk <11366345+PabloKowalczyk@users.noreply.github.com>
2020-04-13 12:42:04 +0200

    Add "\Crunz\Application\Service\ClosureSerializerInterface"

---
+++ src/Application/Service/ClosureSerializerInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Crunz\Application\Service;
+
+interface ClosureSerializerInterface
+{
+    public function serialize(\Closure $closure): string;
+
+    public function unserialize(string $serializedClosure): \Closure;
+}
